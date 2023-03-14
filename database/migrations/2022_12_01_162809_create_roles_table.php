7e99a4ee885ce9c3b27b596308a4eef200ebf419
rahmadar-if <rahmadarifwork@gmail.com>
2023-03-14 10:45:32 +0700

    role fresh seed

--- database/migrations/2022_12_01_162809_create_roles_table.php
+++ database/migrations/2022_12_01_162809_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
             $table->string('uuid');
             $table->boolean('is_supervisor')->nullable();
